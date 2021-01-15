7231c7e825ea7453bcf29393003a9e1ab4ed55e7
Rune Samyn <rune.samyn@gmail.com>
2021-01-15 15:28:30 +0000

    deel 2 van examen

--- examen_TES/Sources/main.c
+++ examen_TES/Sources/main.c
@@ -5,35 +5,32 @@
 #include <unistd.h>
 #include <time.h>
 
-int counter;
+char* logfile1;
+char* logfile2;
+int writeInterval1;
+int writeInterval2;
 
-int main(int argc, char *argv[])
-{
-    char* logfile;
-    int writeInterval;
+pthread_t thread_id1;
+pthread_t thread_id2;
+
+void* thread1(void* arg)
+{ 
     time_t current_time;
     struct tm * time_info;
     char timeString[22];  // space for "dd-mm-YYYY HH:MM:SS\0"
-
-    if (argc == 3)
+    for(;;)
     {
-        logfile = argv[1];
-        sscanf(argv[2], "%i", &writeInterval);
-        FILE *f = fopen(logfile, "a");
+        FILE *f = fopen(logfile1, "a");
 
         if (f != NULL)
         {
-            for (;;)
-            {
-                printf("save time to file\r\n");
-                time(&current_time);
-                time_info = localtime(&current_time);
+            printf("save time to logfile 1\r\n");
+            time(&current_time);
+            time_info = localtime(&current_time);
 
-                strftime(timeString, sizeof(timeString), "%d/%m/%Y %H:%M:%S\n\r", time_info);
-                fputs(timeString, f);
-                fflush(f);
-                sleep(writeInterval);
-            }
+            strftime(timeString, sizeof(timeString), "%d/%m/%Y %H:%M:%S\n\r", time_info);
+            fputs(timeString, f);
+            fflush(f);
             
         }
         else
@@ -41,6 +38,56 @@ int main(int argc, char *argv[])
             printf("couldn't open/make file\n\r");
         }
         fclose(f);
+        sleep(writeInterval1);
+    }
+    return NULL; 
+}
+void* thread2(void* arg)
+{ 
+    time_t current_time;
+    struct tm * time_info;
+    char timeString[22];  // space for "dd-mm-YYYY HH:MM:SS\0"
+    for(;;)
+    {
+        FILE *f = fopen(logfile2, "a");
+
+        if (f != NULL)
+        {
+            printf("save time to logfile 2\r\n");
+            time(&current_time);
+            time_info = localtime(&current_time);
+
+            strftime(timeString, sizeof(timeString), "%d/%m/%Y %H:%M:%S\n\r", time_info);
+            fputs(timeString, f);
+            fflush(f);
+            
+        }
+        else
+        {
+            printf("couldn't open/make file\n\r");
+        }
+        fclose(f);
+        sleep(writeInterval2);
+    }
+    return NULL; 
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc == 5)
+    {
+        logfile1 = argv[1];
+        sscanf(argv[2], "%i", &writeInterval1);
+        logfile2 = argv[3];
+        sscanf(argv[4], "%i", &writeInterval2);
+
+        if (pthread_create(&thread_id1, NULL, &thread1, NULL) != 0) 
+            printf("\nThread 1 can't be created."); 
+        if (pthread_create(&thread_id2, NULL, &thread2, NULL) != 0) 
+            printf("\nThread 2 can't be created."); 
+  
+        pthread_join(thread_id1, NULL); 
+        pthread_join(thread_id2, NULL); 
     }
     else
     {
